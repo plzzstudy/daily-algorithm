67b7d2e3d15ef87645bd59a78774ea2b6c3e4688
chaerin <cr434972@gmail.com>
2021-05-10 05:29:44 +0900

    점수 계산

---
+++ chaerin/inflearn/2-10.py
@@ -0,0 +1,28 @@
+# 점수 계산
+
+# 1번 문제가 맞는 경우 1점
+# 앞의 문제에 대해서 답이 틀리다가 답이 맞는 처음 문제는 1점
+# 연속으로 문제의 답이 맞는 경우
+# 두 번째 문제는 2점, 세 번째 문제는 3점, ..., K 번째 문제는 K점
+
+# 시험 문제의 채점 결과가 주어졌을 때,(1 : 답이 맞는 경우, 0 : 답이 틀린 경우)
+# 총 점수를 계산하는 프로그램을 작성하시오.
+
+N = int(input())
+result = list(map(int, input().split()))
+
+record = []
+if result[0] == 1:
+    record.append(1)
+else:
+    record.append(0)
+
+for i in range(1, N):
+    if result[i] == 1:
+        if record[-1] != 0:
+            record.append(record[-1]+1)
+        else:
+            record.append(1)
+    else:
+        record.append(0)
+print(sum(record))
