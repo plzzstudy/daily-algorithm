6f744b4ee6cfc4689cf137bbce0ed818b54a8125
yangahh <toaur6802@naver.com>
2021-05-25 22:47:39 +0900

    day23: 스택(후위표기식, 후위식 연산)

---
+++ hanah/inflearn/section5-04-Stack_postfix_cal.py
@@ -0,0 +1,34 @@
+'''
+# 후위식 연산
+
+후위연산식이 주어지면 연산한 결과를 출력하는 프로그램을 작성하세요.
+만약 3*(5+2)-9 을 후위연산식으로 표현하면 352+*9- 로 표현되며 그 결과는 21입니다.
+
+▣ 입력설명
+첫 줄에 후위연산식이 주어집니다. 연산식의 길이는 50을 넘지 않습니다. 식은 1~9의 숫자와 +, -, *, /, (, ) 연산자로만 이루어진다.
+
+▣ 출력설명
+연산한 결과를 출력합니다.
+
+▣ 입력예제 1 
+352+*9-
+
+▣ 출력예제 1 
+12
+
+'''
+
+expression = input()
+stack = []
+
+# solution1
+for s in expression:
+    if s.isdecimal():
+        stack.append(s)
+    else:
+        y = stack.pop()
+        x = stack.pop()
+        res = str(eval(x + s + y))
+        stack.append(res)
+
+print(res)
