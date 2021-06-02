cd6bc72eed44d0af34add3519d2edbeb93369b30
yangahh <toaur6802@naver.com>
2021-06-02 23:52:49 +0900

    day30: 수열 추측

--- hanah/inflearn/section6-09-DFS_sequence.py
+++ hanah/inflearn/section6-09-DFS_sequence.py
@@ -27,3 +27,24 @@ N은 가장 윗줄에 있는 숫자의 개수를 의미하며 F는 가장 밑에
 3 1 2 4
 
 '''
+n, f = map(int, input().split())
+p=[0]*n
+b=[1]*n
+ch=[0]*(n+1)
+for i in range(1, n):
+    b[i]=b[i-1]*(n-i)//i
+
+def DFS(L, sum):
+    if L==n and sum==f:
+        for x in p:
+            print(x, end=' ')
+        sys.exit(0)
+    else:
+        for i in range(1, n+1):
+            if ch[i]==0:
+                ch[i]=1
+                p[L]=i
+                DFS(L+1, sum+(p[L]*b[L]))
+                ch[i]=0
+
+DFS(0, 0)
