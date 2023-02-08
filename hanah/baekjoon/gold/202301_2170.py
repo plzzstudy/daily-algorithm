9c3cfb75b16e2d73e8e65eb515007a0e9df320b2
yangahhh <hannah@kasacorp.com>
2023-02-08 18:42:49 +0900

    [Baekjoon] 2170. 선 긋기- 최적화 version

--- hanah/baekjoon/gold/202301_2170.py
+++ hanah/baekjoon/gold/202301_2170.py
@@ -2,27 +2,18 @@
 
 import sys
 
-n = int(input())
 input = sys.stdin.readline
-input_list = [tuple(map(int, input().rstrip().split())) for _ in range(n)]
-input_list.sort()
+n = int(input())
+input_list = sorted([tuple(map(int, input().rstrip().split())) for _ in range(n)], key=lambda x: x[0])
 
-total_len = 0
-for i in range(n):
-    if i == 0:
-        start, end = input_list[i]
-        total_len = end - start
-    else:
-        if input_list[i][0] > start:
-            if input_list[i][0] < end < input_list[i][1]:
-                total_len += input_list[i][1] - end
-                end = input_list[i][1]
-            elif input_list[i][0] >= end:
-                total_len += input_list[i][1] - input_list[i][0]
-                start, end = input_list[i]
-        else:
-            if input_list[i][1] > end:
-                total_len += input_list[i][1] - end
-                end = input_list[i][1]
+start, end = input_list[0]
+total_len = end - start
+for x, y in input_list[1:]:
+    if end <= x:
+        total_len += y - x
+        start, end = x, y
+    elif end < y:
+        total_len += y - end
+        end = y
 
 print(total_len)
